c64f7de7273d7f95d7d8970a26f8fa18ba5da7c5
MyMac <mymac@Jaspers-Mac-Mini.local>
2024-01-16 19:21:17 +0800

    setup locales for app translation

---
+++ lib/generated/locales.g.dart
@@ -0,0 +1,27 @@
+// DO NOT EDIT. This is code generated via package:get_cli/get_cli.dart
+
+// ignore_for_file: lines_longer_than_80_chars
+// ignore: avoid_classes_with_only_static_members
+class AppTranslation {
+  static Map<String, Map<String, String>> translations = {
+    'en': Locales.en,
+    'fr': Locales.fr,
+  };
+}
+
+class LocaleKeys {
+  LocaleKeys._();
+  static const strSignIn = 'strSignIn';
+  static const strSignUp = 'strSignUp';
+}
+
+class Locales {
+  static const en = {
+    'strSignIn': 'Sign In',
+    'strSignUp': 'Sign Up',
+  };
+  static const fr = {
+    'strSignIn': 'Se connecter',
+    'strSignUp': 'S\'inscrire',
+  };
+}
